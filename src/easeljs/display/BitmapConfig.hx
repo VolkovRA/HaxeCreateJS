0387a24a80a78203cb53c67a02fbe3c682dda851
VolkovRA <flash21.ru@gmail.com>
2020-04-05 06:46:11 +0300

    ADD Bitmap

---
+++ src/easeljs/display/BitmapConfig.hx
@@ -0,0 +1,15 @@
+package easeljs.display;
+
+import haxe.extern.EitherType;
+
+/**
+ * Object with a getImage method that returns a CanvasImageSource, or a string URL to an image.
+ * @author VolkovRA
+ */
+typedef BitmapConfig =
+{
+	/**
+	 * Returns a CanvasImageSource, or a string URL to an image.
+	 */
+	var getImage():EitherType<CanvasImageSource, String>;
+}
