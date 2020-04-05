f8814046721126813426a7daa0b3ccae8719240e
VolkovRA <flash21.ru@gmail.com>
2020-04-05 04:04:21 +0300

    ADD Tween config

---
+++ src/easeljs/tween/TimelineConfig.hx
@@ -0,0 +1,13 @@
+package easeljs.tween;
+
+/**
+ * The configuration properties to apply to Timeline instance (ex. {loop:-1, paused:true}).
+ * These props are set on the corresponding instance properties except where specified.
+ * 
+ * Documentation: https://www.createjs.com/docs/tweenjs/classes/Timeline.html
+ * @author VolkovRA
+ */
+typedef TimelineConfig =
+{
+	>AbstractTweenConfig,	
+}
